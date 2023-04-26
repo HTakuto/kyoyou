617c8af86e986f35a6c1dcdd1b3ec5f810f08b46
Takuto <htakutolife@gmail.com>
2023-04-26 20:16:04 +0900

    記事データをDBから取得

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticleController extends Controller
 {
     public function index()
     {
+        $articles = Article::all()->sortByDesc('created_at');
+        
         return view('articles.index', ['articles' => $articles]);
     }
 }
