c49cecc0493e5444f5f50731eafdde97ea8f5291
Takuto <htakutolife@gmail.com>
2023-05-02 01:46:37 +0900

    footer追加

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+    <div class="footer__container">
+      <div class="footer__item">
+        <ul class="footer__list">
+        <h4 class="footer__heading">教YOU</h4>
+        <ul class="footer__list">
+          <li><span>©2023 Takuto.</span></li>
+        </ul>
+      </div>
+      <div class="footer__item">
+        <h4 class="footer__heading">About</h4>
+        <ul class="footer__list">
+          <li><a href="#">教YOUについて</a></li>
+          <li><a href="#">利用規約</a></li>
+          <li><a href="#">プライバシーポリシー</a></li>
+          <li><a href="#">お問い合わせ</a></li>
+        </ul>
+      </div>
+      <div class="footer__item">
+        <h4 class="footer__heading">Developer</h4>
+        <ul class="footer__list">
+          <li><a href="https://htakuto.github.io/portfolio-site/">Portfolio</a></li>
+          <li><a href="https://twitter.com/krt_takuto">Twitter</a></li>
+          <li><a href="https://qiita.com/htakutolife">Qiita</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
