b38ca7bdc7955ffaa98760b873e609934a4e85e7
Takuto <htakutolife@gmail.com>
2023-05-04 21:27:55 +0900

    プロフィール更新ビュー作成

---
+++ resources/views/profiles/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('app')
+
+@section('title', 'プロフィール更新')
+
+@include('nav')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="card mt-3">
+          <div class="card-body pt-0">
+            @include('error_card_list')
+            <div class="card-text">
+              <form method="POST" action="{{ route('profiles.update', ['profile' => $profile]) }}" enctype="multipart/form-data">
+                @csrf
+                @method('PATCH')
+                @include('profiles.form')
+                <button type="submit" class="btn blue-gradient btn-block">更新する</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
