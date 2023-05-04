b6660f3c58e2a0015ea72fcdf778c16a63f14af0
Takuto <htakutolife@gmail.com>
2023-05-04 12:48:10 +0900

    プロフィールモデル

---
+++ app/Profile.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'user_image',
+        'age',
+        'gender',
+        'school_type',
+        'grade',
+        'subject',
+        'club',
+        'comment'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
