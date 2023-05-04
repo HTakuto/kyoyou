6d1090ace9690636551ead3f4e3337e3bfc72fa3
Takuto <htakutolife@gmail.com>
2023-05-04 23:46:06 +0900

    記事投稿の文字列を授業仕様に変更

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,6 +1,6 @@
 @csrf
 <div class="md-form">
-  <label>授業タイトル</label>
+  <label>タイトル</label>
   <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
@@ -12,7 +12,7 @@
 </div>
 <div class="form-group">
   <label></label>
-  <textarea name="body" required class="form-control" rows="16" placeholder="授業の内容を簡潔に記述してください">{{ $article->body ?? old('body') }}</textarea>
+  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
 <div class="form-group">
     <label for="pdf_file">PDFファイル</label>
