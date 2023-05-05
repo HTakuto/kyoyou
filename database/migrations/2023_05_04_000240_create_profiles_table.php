6154854d971d4dac4767748f4fb601694e4bd829
Takuto <htakutolife@gmail.com>
2023-05-05 10:44:08 +0900

    文字列に修正

--- database/migrations/2023_05_04_000240_create_profiles_table.php
+++ database/migrations/2023_05_04_000240_create_profiles_table.php
@@ -20,7 +20,7 @@ class CreateProfilesTable extends Migration
             $table->enum('age', ['20代', '30代', '40代', '50代', '60代', '70代', '80代'])->nullable();
             $table->string('gender')->nullable();
             $table->enum('school_type', ['小学校', '中学校', '高校'])->nullable();
-            $table->integer('school_year')->nullable();
+            $table->string('school_year')->nullable();
             $table->string('subject')->nullable();
             $table->string('club')->nullable();
             $table->text('comment')->nullable();
