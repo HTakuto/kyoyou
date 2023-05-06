3762f70cce3199de3c346124e9c67621addf3201
Takuto <htakutolife@gmail.com>
2023-05-06 18:27:01 +0900

    モデルNotifiable追加

--- app/Comment.php
+++ app/Comment.php
@@ -4,9 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Notifications\Notifiable;
 
 class Comment extends Model
 {
+    use Notifiable;
+
     protected $fillable = [
         'content', 'user_id', 'article_id'
     ];
