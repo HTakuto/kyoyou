573f650914a458daebe32edf8f2a96c8ba1140af
Takuto <htakutolife@gmail.com>
2023-05-13 22:08:03 +0900

    Googleユーザー登録ビュー

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,11 @@
             <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
 
             @include('error_card_list')
-            
+
+            <a href="{{ route('register.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
+                <i class="fab fa-google mr-1"></i>Googleでユーザー登録
+            </a>
+
             <div class="card-text">
               <form method="POST" action="{{ route('register') }}">
                 @csrf
