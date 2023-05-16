101cf8e58e566c7f0f5ac19933d072f858738895
Takuto <htakutolife@gmail.com>
2023-05-17 01:26:38 +0900

    ビューの修正

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -33,25 +33,29 @@
             <span class="notification-badge">{{ $unreadNotificationsCount }}</span>
           @endif
         </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-          <h6 class="dropdown-header">通知</h6>
-          @if ($notifications->isEmpty())
-            <a class="dropdown-item" href="#">通知はありません</a>
-          @else
-            @foreach ($notifications as $notification)
-              @unless($notification->read_at)
-              <a class="dropdown-item" href="{{ $notification->data['url'] }}">
-              @if ($notification->type === 'like')
-                  <a href="{{ route('articles.show', ['article' => $notification->notifiable_id]) }}">{{ \App\Article::find($notification->notifiable_id)->title }}</a>が<a href="{{ route('users.show', ['name' => $notification->causedByUser->name]) }}">{{ $notification->causedByUser->name }}</a>さんにいいねされました。
-              @elseif ($notification->type === 'comment')
-                  <a href="{{ route('articles.show', ['article' => $notification->notifiable_id]) }}">{{ \App\Article::find($notification->notifiable_id)->title }}</a>が<a href="{{ route('users.show', ['name' => $notification->causedByUser->name]) }}">{{ $notification->causedByUser->name }}</a>さんにコメントされました。
-              @elseif ($notification->type === 'follow')
-                  <a href="{{ route('users.show', ['name' => $notification->causedByUser->name]) }}">{{ $notification->causedByUser->name }}</a>さんにフォローされました。
-              @endif
-              </a>
-              @endunless
-            @endforeach
-          @endif
+        <div class="dropdown-menu dropdown-menu-right nav-notification" aria-labelledby="navbarDropdownMenuLink" style="min-width: 60vw;">
+            <h6 class="dropdown-header">通知</h6>
+            @if ($notifications->isEmpty())
+                <a class="dropdown-item" href="#">通知はありません</a>
+            @else
+                @foreach ($notifications as $notification)
+                    @unless($notification->read_at)
+                        <hr>
+                        <div class="py-2 px-4 hover:bg-gray-100">
+                            <div class="mb-1">
+                                @if ($notification->type === 'like')
+                                    <a href="{{ route('articles.show', ['article' => $notification->notifiable_id]) }}">{{ \App\Article::find($notification->notifiable_id)->title }}</a><a>が</a><a href="{{ route('users.show', ['name' => $notification->causedByUser->name]) }}">{{ $notification->causedByUser->name }}</a><a>さんにいいねされました。</a>
+                                @elseif ($notification->type === 'comment')
+                                    <a href="{{ route('articles.show', ['article' => $notification->notifiable_id]) }}">{{ \App\Article::find($notification->notifiable_id)->title }}</a><a>が</a><a href="{{ route('users.show', ['name' => $notification->causedByUser->name]) }}">{{ $notification->causedByUser->name }}</a><a>さんにコメントされました。</a>
+                                @elseif ($notification->type === 'follow')
+                                    <a href="{{ route('users.show', ['name' => $notification->causedByUser->name]) }}">{{ $notification->causedByUser->name }}</a><a>さんにフォローされました。</a>
+                                @endif
+                            </div>
+                            <div class="text-gray-400 text-xs text-right" style="margin-top: -0.5rem;">{{ $notification->created_at->diffForHumans() }}</div>
+                        </div>
+                    @endunless
+                @endforeach
+            @endif
         </div>
       </li>
       @endauth
