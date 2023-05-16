1878a7eaf5ac6fcbba2e8a98e4bf514e63c831f5
Takuto <htakutolife@gmail.com>
2023-05-16 23:14:57 +0900

    プロバイダー作成

---
+++ app/Providers/NotificationsServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Notification;
+use Illuminate\Support\Facades\Auth;
+
+class NotificationsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('nav', function ($view) {
+            $user = Auth::user();
+            $unreadNotificationsCount = 0;
+            $notifications = [];
+
+            if ($user) {
+                $unreadNotificationsCount = Notification::where('user_id', $user->id)
+                    ->where('read_at', false)
+                    ->count();
+
+                $notifications = Notification::where('user_id', $user->id)
+                    ->with('causedByUser')
+                    ->latest()
+                    ->limit(5)
+                    ->get();
+            }
+
+            $view->with('unreadNotificationsCount', $unreadNotificationsCount)
+                ->with('notifications', $notifications);
+        });
+    }
+}
